f67ab57c4480e211567aebaf006a7f4d243add12
Alicia Guardeño <alicia.guardenoalbertos@gmail.com>
2019-09-18 17:14:42 +0200

    Added CSharp template project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bamboohr_jobtest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
